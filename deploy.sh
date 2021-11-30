8671daf4ebcd954a8f9434d511e59128b202b872
LordKalmed <angust.cloud@gmail.com>
2021-11-30 16:25:26 +0000

    removed testing from deplot.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ pip3 install -r test_requirements.txt
 
 
 docker-compose up -d
-sleep 15
+sleep 10
 docker-compose ps
 docker-compose logs nginx
 docker-compose logs service1
@@ -16,4 +16,4 @@ docker-compose logs service3
 docker-compose logs service4
 docker-compose down --rmi all
 
-python3 -m pytest --cov=app
+
