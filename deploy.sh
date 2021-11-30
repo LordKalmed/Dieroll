980b39114228c78ca4cb7da5ea1f78b842474d49
LordKalmed <angust.cloud@gmail.com>
2021-11-30 13:34:00 +0000

    added deploy.sh to the files

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+
+docker-compose up -d
+docker-compose ps
+docker-compose logs nginx
+docker-compose logs service1
+docker-compose logs service2
+docker-compose logs service3
+docker-compose logs service4
+docker-compose down --rmi all
