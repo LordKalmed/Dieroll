8ac32f4f6a162aa21a5be76d0294bffa8d6577da
LordKalmed <angust.cloud@gmail.com>
2021-12-02 15:40:12 +0000

    changes to docker compose. Should now push to repo

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ sudo apt install python3-pip
 
 pip3 install -r test_requirements.txt
 
-
+docker-compose build
 docker-compose up -d
 sleep 10
 docker-compose ps
@@ -15,6 +15,9 @@ docker-compose logs service2
 docker-compose logs service3
 docker-compose logs service4
 docker-compose down --rmi all
+docker-compose push
+
+
 
 cd /home/angus
 ansible-playbook -i inventory.yaml playbook.yaml
